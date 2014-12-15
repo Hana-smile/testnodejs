d49aa740f7f30e56e5fccc74ca34d1771c393c39
xiaoqinhan <xiaoqinhan@WSWINCNHZ0869.hengtiansoft.com>
2014-12-15 16:40:56 +0800

    bootstrap less

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		less:{
+			compileLogin: {
+				option:{ },
+				files: {
+					'public/css/login.css':'public/less/login.less'
+				}
+			},
+			minify : {
+                options : {
+                    cleancss : true,
+                    report : 'min',
+                    compress : true,
+                    cleancssOptions : {
+                        keepSpecialComments : 0,
+                        keepBreaks : false
+                    }
+                },
+                files : {
+                	'public/css/login.min.css':'public/css/login.css'
+                }
+            }
+		},
+		
+		watch:{
+			scripts: {
+			   files:['public/less/*.less','public/less/*/*.less'],
+			   tasks:['less']
+			} 
+		
+		}
+	});	
+	
+	grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', [ 'less', 'watch' ]);
+    grunt.registerTask('buildAll', [ 'less', 'requirejs', 'uglify' ]);
+
+}
